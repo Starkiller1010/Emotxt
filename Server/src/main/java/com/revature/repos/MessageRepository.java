15e25301e053f0a3a620051b4b646fa74854b831
John Werenski <jtwerenski@gmail.com>
2019-10-10 20:53:46 -0400

    Create Account and Message Repo with CRUD functionailty.

--- Server/src/main/java/com/revature/repos/MessageRepository.java
+++ Server/src/main/java/com/revature/repos/MessageRepository.java
@@ -1,5 +1,52 @@
 package com.revature.repos;
 
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.revature.models.Message;
+
+
 public class MessageRepository {
+	
+	private SessionFactory factory;
+	
+	@Autowired
+	public MessageRepository(SessionFactory sessionFactory) {
+		this.factory = sessionFactory;
+	}
+
+	public List<Message> getAll() {
+		return factory.getCurrentSession().createQuery("from Account", Message.class).getResultList();
+	}
+
+	public Message getById(int id) {
+		return factory.getCurrentSession().get(Message.class, id);
+	}
+
+	public Message save(Message newMessage) {
+		return (Message) factory.getCurrentSession().save(newMessage);
+	}
+
+	public boolean update(Message updatedMessage) {
+		Session session = factory.getCurrentSession();
+		Message persistentMessage = session.get(Message.class, updatedMessage.getId());
+		if(persistentMessage == null) return false;
+		persistentMessage.setAuthor(updatedMessage.getAuthor());
+		persistentMessage.setBody(updatedMessage.getBody());
+		persistentMessage.setTone(updatedMessage.getTone());
+		return true;
+	}
+
+	public boolean deleteById(int id) {
+		Session session = factory.getCurrentSession();
+		Message messageForDeletion = session.get(Message.class, id);
+		if(messageForDeletion == null) return false;
+		session.delete(messageForDeletion);
+		return true;
+	}
+
 
 }
