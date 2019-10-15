738ced861541c59e3ce47d3277f5661ade5df44e
ElderFuthark <40003119+ElderFuthark@users.noreply.github.com>
2019-10-14 20:48:33 -0400

    Added password string to Principal POJO

--- Server/src/main/java/com/revature/dtos/Principal.java
+++ Server/src/main/java/com/revature/dtos/Principal.java
@@ -4,11 +4,13 @@ public class Principal {
 
 	private int id;
 	private String username;
+	private String password;
 	private String role;
-	public Principal(int id, String username, String role) {
+	public Principal(int id, String username, String password, String role) {
 		super();
 		this.id = id;
 		this.username = username;
+		this.password = password;
 		this.role = role;
 	}
 	
@@ -23,6 +25,12 @@ public class Principal {
 	}
 	public void setUsername(String username) {
 		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
 	}
 	public String getRole() {
 		return role;
