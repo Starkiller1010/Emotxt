3437cfa9e89518033ce91bcd3a88e5039c4a8b7c
John Werenski <jtwerenski@gmail.com>
2019-10-16 16:27:42 -0400

    Create a Landing component and add a route for the landing in the router

--- Client/Emotxt/src/app/app-routing.module.ts
+++ Client/Emotxt/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'landing', component: LandingComponent}
 ];
 
 @NgModule({
