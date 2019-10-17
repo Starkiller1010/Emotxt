f6a6e92cf2d155610bb22c8bcb8c717fa0dc208a
ElderFuthark <40003119+ElderFuthark@users.noreply.github.com>
2019-10-17 18:38:57 -0400

    Class files added for Account, Channel and User
    The class files clean up code later by allowing the aformentioned
    objects to be instantiated before being sent via requests to the server.

--- Client/Emotxt/src/app/models/user/user.ts
+++ Client/Emotxt/src/app/models/user/user.ts
@@ -1,9 +1,21 @@
 import { Message } from '../message/message'
 
 export class User {
+    id: number;
     username: String;
     password: String;
     email: String;
-    city: String;
     messages: Array<Message>
+    jwt: String;
+
+    constructor(un: string, pw: string, email: string, 
+        id?: number, messages?: Array<Message>, token?: string)
+    {
+        this.id = id || 0;
+        this.username = un;
+        this.password = pw;
+        this.email = email;
+        this.messages = messages || [];
+        this.jwt = token || "";
+    }
 }
