38ffd4348103e972a93fc27bb8af69a00fe72e3c
ElderFuthark <40003119+ElderFuthark@users.noreply.github.com>
2019-10-17 07:35:18 -0400

    Registration screen mostly working
    The rest of the structure has been put in place. Currently, I'm having
    an issue catching validation errors involving patterns/regex. Once all
    of the validation is set, I can attempt to send data to the application.

--- Client/Emotxt/src/app/components/register/register.component.ts
+++ Client/Emotxt/src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, EmailValidator } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  submitted: boolean;
+
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+      console.log("Instantiating RegisterComponent...");
+      console.log("RegisterComponent instantiation complete");
+   }
 
   ngOnInit() {
+    console.log("ngInit RegisterComponent...");
+    this.submitted = false;
+    this.registerForm = this.formBuilder.group({
+      username: ['', [ Validators.pattern('^[\w[^0-9_]]\w+'), Validators.minLength(6), Validators.required] ],
+      password: ['',[ Validators.minLength(8), Validators.required] ],
+      email: ['', [ EmailValidator, Validators.required] ],
+      country: [''],
+      state: [''],
+      bio: [''],
+    })
   }
 
+  get fields() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit = () => {
+    this.submitted = true;
+    if(this.registerForm.invalid) return
+
+  }
 }
