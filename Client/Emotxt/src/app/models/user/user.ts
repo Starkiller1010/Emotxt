108eab4cc36ce6840985cbea76f256c56022a568
ElderFuthark <40003119+ElderFuthark@users.noreply.github.com>
2019-10-16 22:34:04 -0400

    User Class completed

--- Client/Emotxt/src/app/models/user/user.ts
+++ Client/Emotxt/src/app/models/user/user.ts
@@ -1,2 +1,9 @@
+import { Message } from '../message/message'
+
 export class User {
+    username: String;
+    password: String;
+    email: String;
+    city: String;
+    messages: Array<Message>
 }
